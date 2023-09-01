df5009d843bde293ed3b588c0c7f7eb1d92a561b
RianC4rl0s <riancarlos.sl@gmail.com>
2023-09-01 12:26:08 -0300

    feat:login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Input } from "antd"
+import { Button, Input } from "antd"
 
 export function Login() {
     return (
@@ -8,9 +8,14 @@ export function Login() {
             <div className="bg-slate-500 absolute inset-y-0 right-0 w-1/3  p-8">
                 <div></div>
                 <label htmlFor="Login" className="font-semibold text-white my-2 ">Login</label>
-                <Input size="large"  id="Login" placeholder="Caze"></Input>
+                <Input size="large" id="Login" placeholder="Caze"></Input>
                 <label htmlFor="Password" className="font-semibold text-white my-2">Password</label>
-                <Input.Password  size="large"  id="Password" placeholder="Senha" />
+                <Input.Password size="large" id="Password" placeholder="Senha" />
+                <div className="w-full text-right text-white">
+                    Not registered?
+                    <a href="#" className="font-semibold mx-1 text-[#F52C15] hover:underline transition ease-in-out">Sign in</a>
+                </div>
+                <Button type="primary" className="w-full my-2" >Login</Button>
             </div>
         </div>
     )
