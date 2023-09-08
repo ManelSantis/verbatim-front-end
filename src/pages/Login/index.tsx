9c821f3a1cd1020a5e0623878910e005fa113bda
RianC4rl0s <riancarlos.sl@gmail.com>
2023-09-08 09:10:38 -0300

    feat: login tests

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,21 +2,25 @@ import { Button, Input } from "antd"
 
 export function Login() {
     return (
-        <div>
+        <div className="w-screen h-screen">
+            <div className="bg-really-dark-red absolute inset-y-0 right-0 w-1/3 p-8 flex flex-col justify-center shadow-2xl">
+                <div className=" h-52 w-full flex flex-col items-center justify-center" >
 
-
-            <div className="bg-slate-500 absolute inset-y-0 right-0 w-1/3  p-8">
-                <div></div>
+                    <div className="h-52 w-52 rounded-full bg-slate-400"></div>
+                </div>
                 <label htmlFor="Login" className="font-semibold text-white my-2 ">Login</label>
                 <Input size="large" id="Login" placeholder="Caze"></Input>
                 <label htmlFor="Password" className="font-semibold text-white my-2">Password</label>
-                <Input.Password size="large" id="Password" placeholder="Senha" />
+                <Input.Password size="large" id="Password" placeholder="Password" />
                 <div className="w-full text-right text-white">
                     Not registered?
-                    <a href="#" className="font-semibold mx-1 text-[#F52C15] hover:underline transition ease-in-out">Sign in</a>
+                    <a href="#" className="font-semibold mx-1 text-light-red hover:text-white transition ease-in-out">Sign in</a>
                 </div>
-                <Button type="primary" className="w-full my-2" >Login</Button>
-               
+                <div className="gap-2">
+                    <Button type="primary" className="w-full my-2 hover:bg-light-red transition ease-in-out" >Login</Button>
+                    <Button>Login with Google</Button>
+                    <Button >Login with Facebook</Button>
+                </div>
             </div>
         </div>
     )
